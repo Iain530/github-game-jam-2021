b9ef6fa5a4ad644f65f4db8c9ee9c2b832120cfa
Iain Cattermole <iain_c@live.co.uk>
2021-11-12 21:31:00 +0000

    Add Player

--- GitHub Game Jam 2021/Assets/Scripts/GameState/GameState.cs
+++ GitHub Game Jam 2021/Assets/Scripts/GameState/GameState.cs
@@ -9,6 +9,7 @@ public class GameState {
     public int gameId;
     public Bee[] bees;
     public Task[] tasks;
+    public Player[] players;
 }
 
 
@@ -17,6 +18,7 @@ public class Bee {
     public int id;
     public string name = "";
     public Vector2 position;
+    public bool isPlayer;
 }
 
 
@@ -24,4 +26,10 @@ public class Bee {
 public class Task {
     public int id;
     public bool complete;
+}
+
+
+public class Player {
+    public int id;
+    public bool isQueenBee;
 }
