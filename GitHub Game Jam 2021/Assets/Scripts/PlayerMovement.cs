157619a4b105f11f459571d5b07926b6f1f5dc8a
Darya Shumitskiy <darya.shumitskiy@skyscanner.net>
2021-11-13 00:56:14 +0000

    Move in fixedupdate

--- GitHub Game Jam 2021/Assets/Scripts/PlayerMovement.cs
+++ GitHub Game Jam 2021/Assets/Scripts/PlayerMovement.cs
@@ -9,15 +9,17 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer _renderer;
     private Rigidbody2D _rigidBody;
 
-    // Start is called before the first frame update
     void Start()
     {
         _renderer = GetComponent<SpriteRenderer>();
         _rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+    }
+
+    void FixedUpdate()
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
@@ -31,12 +33,5 @@ public class PlayerMovement : MonoBehaviour
         {
             _renderer.flipX = true;
         }
-
-        // Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
-
-        // movement *= Time.deltaTime;
-
-        // transform.Translate(movement);
-        // _rigidBody.MovePosition(_rigidBody.position + speed * Time.fixedDeltaTime);
     }
 }
