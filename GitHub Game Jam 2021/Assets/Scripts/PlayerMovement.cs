3c39ea6fe19ed56252c0355efdd3d6a01227ec8d
Darya Shumitskiy <darya.shumitskiy@skyscanner.net>
2021-11-13 00:16:23 +0000

    Add movement with flip

---
+++ GitHub Game Jam 2021/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Vector2 speed = new Vector2(50, 50);
+    private SpriteRenderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            _renderer.flipX = false;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            _renderer.flipX = true;
+        }
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
