57c84a75fbabde5292f242b5bc3a94152e648b21
yasmojam <yazzij97@gmail.com>
2021-11-13 20:46:02 +0000

    Path finding

--- GitHub Game Jam 2021/Assets/Scripts/PlayerMovement.cs
+++ GitHub Game Jam 2021/Assets/Scripts/PlayerMovement.cs
@@ -21,17 +21,18 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        BeeNetworkClient.Instance.SendCurrentPosition(gameObject.transform.position);
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
         _rigidBody.velocity = new Vector2(inputX * speed, inputY * speed);
 
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
-            _renderer.flipX = false;
+            _renderer.flipX = true;
         }
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            _renderer.flipX = true;
+            _renderer.flipX = false;
         }
     }
 }
